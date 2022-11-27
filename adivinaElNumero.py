fb04d738e1c03a55095fa8004e717b862bf1ced7
DanielSalvadorp <drante81@gmail.com>
2022-11-27 03:36:54 -0500

    funciones random

--- adivinaElNumero.py
+++ adivinaElNumero.py
@@ -1,14 +1,16 @@
-from random import randrange 
+from random import randrange, choice
 #inicio
 #Ingresa un numero
 print("""Adivina el número
 Recuerda, cada vez que falles, el número cambiará""")
 def Random():
-    k=int(input("Inregresa un numero: "))
-    s=randrange(7)
+    k=int(input("Inregresa un numero del 0 al 10: "))
+    s=randrange(10)
+    #print(s)
     if k==s :
         print("¡Correcto!")
     else:
         print("va de nuevo")
         Random()
 Random()
+
